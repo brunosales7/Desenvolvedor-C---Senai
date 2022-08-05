60972fa1418f2d188a2142183efa5c4f8da61a5c
brunosales7 <bruno_sales7@hotmail.com>
2022-08-05 11:09:51 -0300

    first commit

---
+++ ApostilaDeCSharp.ParadigmaEstruturado/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApostilaDeCSharp.ParadigmaEstruturado
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * 
+             * Sequêncial - TopDown
+             * 
+             * passo 1
+             * passo 2
+             * passo 3
+             * passo 4
+             * 
+             */
+            //Passo 1 -Entrada de Dados
+
+            string repetir;
+            do
+            {
+                Console.WriteLine("Qual o aluno? ");
+                string nome = Console.ReadLine().ToUpper();
+                //Passo 2 - Processamento
+                string resposta;
+                if (nome.Equals("REGYS"))
+                {
+                    resposta = "Nosso Representante";
+                }
+                else
+                {
+                    resposta = ("Chame o Representante! ");
+                }
+                //Passo 3 -Saida
+
+                Console.WriteLine(resposta);
+
+                //PASSO 4 - MONTAR UMA ITERAÇÃO (REPETIÇÃO)-LOOPING
+                Console.WriteLine("Deseja sair? SIM OU NÃO");
+                repetir = Console.ReadLine().ToUpper();
+
+
+            } while (!repetir.Equals("SIM"));//         CDSFF     
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ SomaDeNotas/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomaDeNotas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] notas = new int[4];
+            notas[0] = 9;
+            notas[1] = 21;
+            notas[2] = 7;
+            notas[3] = 15;
+            Console.WriteLine("mostrar as notas");
+            double soma = 0;
+            for(int i = 0; i < 4; i++)
+            {
+                Console.WriteLine(notas[i]);
+                soma= soma + notas[i];
+            }
+            double media = soma / notas.Length;
+            Console.WriteLine($"soma: {soma}");
+          //Console.WriteLine($"media:{media}");
+            Console.WriteLine("Sua media é: " + media);
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
